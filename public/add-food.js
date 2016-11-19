f28ca46ae77a7bfd7b546babc3b66f8e0909151c
mjtamayo <mariajose.tamayo@gmail.com>
2016-11-19 01:46:04 +0000

    added recommendation

--- public/add-food.js
+++ public/add-food.js
@@ -17,6 +17,11 @@ $(document).ready(function () {
   var datePicker = $('#date');
   var mealType = $('#mealType');
   var saveMealButton = $('#saveBtn');
+  var micronutrientsDiv = $('#micronutrientsDiv');
+  var foodInfoDiv = $('.foodInfo')
+  var foodInfoTitle = $('#foodInfoTitle');
+  
+  /////// Requests ////////
   
   var onSaveMeal = function (name, date, meal){
     name = foodName.val();
@@ -34,15 +39,127 @@ $(document).ready(function () {
         contentType: 'application/json'
     });
     ajax.done(function(res){
-      console.log("this is the response", res)
+      getFoodRequest(res.name)
     });
   };
+  
+  /////// Requests to Nutritionix ////////
+  
+  function getFoodRequest(searchTerm){
+
+		var searchTerm= searchTerm;
+		var data = {
+ 			"query": searchTerm,
+  			"timezone": "US/Eastern",
+		};
+
+		$.ajax({
+  			url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+  			type: 'POST',
+  			data: JSON.stringify(data),
+  			headers: {
+    			"Content-Type" : "application/json",
+    			"x-app-id": "c7faf842",  
+    			"x-app-key": "bc4a198b34d738f0f52d5f874775d99d",  
+  			},
+  			dataType: 'json',
+  			success: function (data) {
+    			// Este console.log te muestra el objeto que retornas
+    			console.info(data);
+    
+    			//Mostrar elemento en pantalla
+    			showNutritionalValue(data);
+    			var isRecommended = isFoodRecommended(data);
+				  showRecomendation(isRecommended);
+  			},
+  			error: function (request, status, error) {
+    			// Estos console.log te dan informacion del error.
+    			console.log(request.responseText);
+    			console.log(error);
+    			console.log(status);
+    		// 	var errorElem = showError(error);
+  			},
+		});
+	}
+	
+	function showNutritionalValue(data){
+//   // Funcion que contiene el jQuery para mostrar en la pantalla.
+// 		// Esto es la separacion de concerns. En la de arriba se hace el query, en
+// 		// esta se muestra todo en pantalla.
+// 		document.getElementById("foodItem").value = "";
+// 		document.getElementById("foodItem").value = "";
+// 		$("#foodPhoto").attr('src', data.foods[0].photo.thumb);
+		foodInfoTitle.text('Information for:' + ' ' + data.foods[0].food_name);
+
+		// for (var i=0; i< data.foods[0].full_nutrients.length; i++){
+		// 	var nutrient = data.foods[0].full_nutrients[i];
+		// 	var nutrientID = nutrient.attr_id;
+		// 	// console.log(nutrientID); te imprime toodos los ides de los full_nutrients, un opor uno.
+		// 	if (window.nutrientField[nutrientID]) {
+		// 		var name = window.nutrientField[nutrientID].name;
+		// 		if (nutrient.value !== 0) {
+		// 			micronutrientsDiv.append(
+		// 				"<li>" + name + ": " + nutrient.value + " " + window.nutrientField[nutrientID].unit + "</li>"
+		// 			);
+		// 		}
+		// 	}
+		// }
+
+		$(".performance-facts__title").text("Nutrition Information       "+ data.foods[0].serving_weight_grams + " grams");
+		$("#calories").text(data.foods[0].nf_calories + " Kcal");
+		$("#total_fat").text(data.foods[0].nf_total_fat + " g");
+		$("#saturated_fat").text(data.foods[0].nf_saturated_fat + " g");
+		$("#cholesterol").text(data.foods[0].nf_cholesterol + "mg");
+		$("#sodium").text(data.foods[0].nf_sodium + " mg");
+		$("#total_carbs").text(data.foods[0].nf_total_carbohydrate + " g");
+		$("#dietary_fiber").text(data.foods[0].nf_dietary_fiber + " g");
+		$("#sugar").text(data.foods[0].nf_sugars + " g");
+		$("#protein").text(data.foods[0].nf_protein + " g");
+	
+	}
+	
+	function isFoodRecommended(data){
+		var isRecommended = true;
+		for (var i=0; i< data.foods[0].full_nutrients.length; i++){
+			var nutrient = data.foods[0].full_nutrients[i];
+			var nutrientID = nutrient.attr_id;
+			// console.log(nutrientID); te imprime toodos los ides de los full_nutrients, un opor uno.
+			if(nutrientID === 305){
+			 	var phosphorus = nutrient.value;
+				console.log(phosphorus);
+			}
+			if(nutrientID === 605){
+			 	var transFat = nutrient.value;
+				console.log(phosphorus);
+			}
+		}
+
+		if ((data.foods[0].nf_total_carbohydrate>=60) || (data.foods[0].nf_calories> 200) || (data.foods[0].serving_weight_grams<15 && data.foods[0].nf_calories> 30) || (data.foods[0].nf_saturated_fat>3) || (transFat>0) || (data.foods[0].serving_weight_grams<15 && data.foods[0].nf_calories> 30)){
+			isRecommended = false;
+		};
+		return isRecommended;
+	};
+
+	function showRecomendation(isRecommended){
+		$("#results2").empty()
+		if (isRecommended){
+		// 	$(".resultsContainer").addClass("recommendedContainer");//Pintar de verde el div
+		// 	$(".resultsContainer").removeClass("notRecommendedContainer");
+			micronutrientsDiv.append("<h2> Recommended!!! </h2>");
+		} else  {
+			// Aparezca texto "Not Rec"
+		// 	$("#results2").append("<h2> Not Recommended!!! </h2>");
+		// 	$(".resultsContainer").addClass("notRecommendedContainer");//Pintarle de rojo al texto.
+			micronutrientsDiv.removeClass("recommendedContainer");
+		}
+	};
   
   /////// Events Listeners ///////
   
   saveMealButton.click(function(event){
     event.preventDefault();
     onSaveMeal();
+    
     foodName.val('');
     datePicker.val('');
     mealType.val('');
